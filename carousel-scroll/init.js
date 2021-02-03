9bb34339eeb7caef51089ffa450c1574ee87818d
michaelspellacy <spell@spellacy.net>
2021-02-02 19:51:53 -0500

    Nav

---
+++ carousel-scroll/init.js
@@ -0,0 +1,10 @@
+/*!
+
+  Radancy Pattern Library: Carousel Scroll
+
+  Contributor(s):
+  Michael "Spell" Spellacy, Email: michael.spellacy@radancy.com, Twitter: @spellacy, GitHub: michaelspellacy
+
+  Dependencies: None
+
+*/
