42a74316a7e410ee621904447b61db90a5e9e2b5
michaelspellacy <spell@spellacy.net>
2021-02-11 16:29:55 -0500

    Naming convention updates, formatting, etc.

--- disclosure/init.js
+++ disclosure/init.js
@@ -7,36 +7,64 @@
 
 */
 
-var getExpanders = document.getElementsByClassName("prod-expander");
+var getExpanders = document.getElementsByClassName("disclosure");
 var simpleExpander = {
+
   init: function(){
+
     console.log("got here");
+
     // add listener
+
     var i = 0;
+
     for (i = 0; i < getExpanders.length; i++) {
+
+      getExpanders[i].setAttribute("aria-expanded", "false");
       getExpanders[i].addEventListener("click", simpleExpander.clicked);
+
     }
-  },
-  clicked: function(){
+
+  }, clicked: function(){
+
     // run click event
+
     var isActive = this.getAttribute("aria-expanded");
     var labelOpen = this.getAttribute("data-label-open");
     var labelClose = this.getAttribute("data-label-close");
+
     if (isActive == "true") {
+
       // active
+
       this.setAttribute("aria-expanded", "false");
+
       if( labelOpen.trim() ) {
+
         this.innerHTML = labelOpen;
+
       }
+
     } else {
+
       // not active
+
       this.setAttribute("aria-expanded", "true");
+
       if( labelClose.trim() ) {
+
         this.innerHTML = labelClose;
+
       }
+
     }
+
   }
+
 }
+
 if(getExpanders){
+
   simpleExpander.init();
+
 }
