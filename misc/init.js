e50def8966b00a9c93b1520e83c01a2e803a3246
Michael Spellacy (Spell) <spell@spellacy.net>
2022-06-28 18:00:02 -0400

    For Paul

---
+++ misc/init.js
@@ -0,0 +1,53 @@
+/*!
+
+  Radancy Pattern Library: Miscellaneous Examples
+
+  Contributor(s):
+  Michael "Spell" Spellacy, Email: michael.spellacy@radancy.com, Twitter: @spellacy, GitHub: michaelspellacy
+  Dependencies: None
+
+*/
+
+// Example 1: Your Path
+
+var yourPathClass = ".your-path";
+var yourPathBtnClass = ".your-path__btn";
+var yourPathContentClass = ".your-path__content";
+var yourPath = document.querySelectorAll(yourPathClass);
+var yourPathBtn = document.querySelectorAll(yourPathBtnClass);
+var yourPathContent = document.querySelectorAll(yourPathContentClass);
+
+yourPathContent.forEach(function(content, e){
+
+  content.setAttribute("id",  "your-path-" + (e + 1));
+
+});
+
+yourPathBtn.forEach(function(button, e){
+
+  button.setAttribute("aria-controls", "your-path-" + (e + 1));
+  button.setAttribute("aria-expanded", "false");
+
+  button.addEventListener("click", function () {
+
+    if(this.getAttribute("aria-expanded") === "true") {
+
+      this.setAttribute("aria-expanded", "false");
+
+    } else {
+
+      var activeButton = this.closest(yourPathClass).querySelectorAll(yourPathBtnClass);
+
+      activeButton.forEach(function(active, i){
+
+        active.setAttribute("aria-expanded", "false");
+
+      });
+
+      this.setAttribute("aria-expanded", "true");
+
+    }
+
+  });
+
+});
