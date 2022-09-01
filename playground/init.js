7d997a5ffdf98db2c8e5ad8e7e230a245d115969
Michael Spellacy (Spell) <spell@spellacy.net>
2022-09-01 15:57:11 -0400

    Nav Example

---
+++ playground/init.js
@@ -0,0 +1,16 @@
+/*!
+
+  Radancy Pattern Library: Playground
+
+  Contributor(s):
+  Michael "Spell" Spellacy, Email: michael.spellacy@radancy.com, Twitter: @spellacy, GitHub: michaelspellacy
+
+  Dependencies: jQuery
+
+*/
+
+$(".primary-navigation__btn").on("click", function() {
+
+  $(this).parent().toggleClass("active");
+
+});
