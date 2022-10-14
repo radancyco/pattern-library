dc731f4af44e06990adffe38e2785de6a279bba2
Michael Spellacy (Spell) <spell@spellacy.net>
2022-10-14 13:55:16 -0400

    CS Update

---
+++ carousel-scroll/init.js
@@ -0,0 +1,53 @@
+/*!
+
+  Radancy Pattern Library: Carousel Scroll
+
+  Contributor(s):
+  Michael "Spell" Spellacy, Email: michael.spellacy@radancy.com, Twitter: @spellacy, GitHub: michaelspellacy
+
+  Dependencies: None
+
+*/
+
+var carousel = document.querySelector(".carousel-scroll__container");
+var isDown = false;
+var startX;
+var scrollLeft;
+
+carousel.addEventListener("mousedown", function (e) {
+
+  isDown = true;
+  carousel.classList.add("active");
+  startX = e.pageX - carousel.offsetLeft;
+  scrollLeft = carousel.scrollLeft;
+
+});
+
+carousel.addEventListener("mouseleave", function () {
+
+  isDown = false;
+  carousel.classList.remove("active");
+
+});
+
+carousel.addEventListener("mouseup", function () {
+
+  isDown = false;
+  carousel.classList.remove("active");
+
+});
+
+carousel.addEventListener("mousemove", function (e) {
+
+  if(!isDown) return;
+
+  e.preventDefault();
+
+  var x = e.pageX - carousel.offsetLeft;
+  var walk = (x - startX) * 1; // Scroll Speed
+
+  carousel.scrollLeft = scrollLeft - walk;
+
+  console.log(walk);
+
+});
